5a9d1cd638067c27bf993d0080dcbc0e94f25b2a
Denis <plax1denis@mail.ru>
2024-03-30 15:50:24 +0300

    Доработка переходов в разные экраны и добавления возможности лайков и подписок с разных фрагментов 2

--- app/src/main/java/com/example/coffeelove/coffee/CoffeePostAboutFragment.kt
+++ app/src/main/java/com/example/coffeelove/coffee/CoffeePostAboutFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import com.example.coffeelove.R
 import com.example.coffeelove.databinding.FragmentCoffeePostAboutBinding
 
@@ -16,14 +18,18 @@ class CoffeePostAboutFragment : Fragment() {
 
     private val mBinding get()=_binding!!
     private val viewModel: CoffeeViewModel by activityViewModels<CoffeeViewModel>()
+    lateinit var coffeePost:CoffeePost
+
+
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val coffeePost :CoffeePost = viewModel.getMoreCoffeePostAboutFragment()!!
         _binding= FragmentCoffeePostAboutBinding.inflate(inflater,container,false)
+        coffeePost = viewModel.getMoreCoffeePostAboutFragment()!!
         mBinding.userNameText.text=coffeePost.userNickname
         mBinding.coffeeRecipeName.text=coffeePost.recipeName
         mBinding.recipeDescription.text=coffeePost.recipeDescription
@@ -34,6 +40,33 @@ class CoffeePostAboutFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mBinding.buttonGoBack.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        mBinding.userInfo.setOnClickListener {
+            if(it!=mBinding.btnAdd){
+                viewModel.downLoadOpenUser(coffeePost.userNickname!!)
+                it.findNavController().navigate(R.id.action_coffeePostAboutFragment_to_accountFragment)
+            }
+        }
+
+        mBinding.btnLike.setOnClickListener {
+            val isContain=viewModel.getFavoritePostID().contains(coffeePost.id)
+            if(isContain){
+                Toast.makeText(requireContext(),"Пост уже добавлен в избранное",Toast.LENGTH_SHORT).show()
+            }else{
+                viewModel.addFavoritePost(coffeePost.id!!)
+                viewModel.getLiveDataFavorite().value?.add(coffeePost)
+            }
+        }
+
+        mBinding.btnAdd.setOnClickListener {
+            val isContain= viewModel.getMySubsLiveData().value?.contains(coffeePost.userNickname)
+            if(isContain==true){
+                Toast.makeText(requireContext(),"Вы уже подписаны",Toast.LENGTH_SHORT).show()
+            }else{
+                viewModel.addSubscribers(coffeePost.userNickname!!)
+                viewModel.getMySubsLiveData().value?.add(coffeePost.userNickname!!)
+            }
         }
     }
 
